76e43891706ec3071fb73fd0030f760c41b84a19
Yber-M <ybermarlon@gmail.com>
2024-08-20 03:56:38 -0500

    v1.0

--- js/scripts.js
+++ js/scripts.js
@@ -17,20 +17,3 @@ function seleccionar() {
     document.getElementById('nav').classList = '';
     menuVisible = false;
 }
-
-// * Aplicar animaciones a las habiliades
-function efectoSkills() {
-    var skills = document.getElementById('skills');
-    var dist_skill = window.innerHeight - skills.getBoundingClientRect().top;
-
-    if (dist_skill >= 300) {
-        let skills = document.getElementsByClassName('progreso');
-        skills[1].classList.add('reactt');
-         
-    }
-}
-
-// * Detectar scrolling para animar
-window.onscroll = function () {
-    efectoSkills();
-}
